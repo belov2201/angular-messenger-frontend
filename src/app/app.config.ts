9b3bb2d7f59b775063209b7bd9790dd5b06adbc2
Андрей Белов <andrew.belov2201@yandex.ru>
2025-12-26 19:03:38 +0300

    Установка tailwind и primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZonelessChangeDetection(), provideRouter(routes)],
+  providers: [
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
+  ],
 };
