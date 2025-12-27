d01f04bd3650b9229b4faccedf8aa409da4edcb5
Андрей Белов <andrew.belov2201@yandex.ru>
2025-12-27 17:31:06 +0300

    Добавление логики взаимодействия с формой авторизации

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,13 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { RouterModule } from '@angular/router';
+import { FormBuilder, FormGroupDirective, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
-  imports: [RouterModule, ButtonModule, CardModule, InputTextModule, FloatLabelModule],
+  imports: [
+    RouterModule,
+    ButtonModule,
+    CardModule,
+    InputTextModule,
+    FloatLabelModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -15,4 +23,19 @@ import { RouterModule } from '@angular/router';
     class: 'h-full flex justify-center items-center',
   },
 })
-export class AuthComponent {}
+export class AuthComponent {
+  fb = inject(FormBuilder);
+
+  authForm = this.fb.group(
+    {
+      login: [''],
+      password: [''],
+    },
+    { updateOn: 'change' },
+  );
+
+  auth(formDirective: FormGroupDirective) {
+    formDirective.resetForm();
+    (document.activeElement as HTMLElement)?.blur();
+  }
+}
