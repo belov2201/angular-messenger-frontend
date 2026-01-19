11c5239759a99c06ad3f819cf33786a127887516
Андрей Белов <andrew.belov2201@yandex.ru>
2026-01-19 14:31:44 +0300

    Добавление playwright и e2e теста авторизации

---
+++ e2e/auth.spec.ts
@@ -0,0 +1,26 @@
+import { expect, test } from '@playwright/test';
+
+test('auth', async ({ page }) => {
+  await page.route('**/auth', async (route) => {
+    await new Promise((r) => setTimeout(() => r(null), 500));
+    await route.continue();
+  });
+
+  await page.goto('/');
+
+  await expect(page.getByText('Авторизация')).toBeVisible();
+
+  const loginField = page.getByLabel('Логин');
+  const passwordField = page.getByLabel('Пароль');
+  const loginBtn = page.getByRole('button', { name: 'Войти' });
+
+  await expect(loginBtn).toBeDisabled();
+
+  await loginField.fill('sgreen');
+  await passwordField.fill('12345678');
+  await loginBtn.click();
+
+  await expect(page.getByTestId('loader')).toBeVisible();
+
+  await expect(page.getByText('Sarah Green')).toBeVisible();
+});
