be316501cacbe761245516aec5f0832b3d25d327
Андрей Белов <andrew.belov2201@yandex.ru>
2026-02-07 18:36:31 +0300

    Тестирование date-to-str pipe

--- src/app/main/pipes/date-to-str/date-to-str.pipe.spec.ts
+++ src/app/main/pipes/date-to-str/date-to-str.pipe.spec.ts
@@ -1,8 +1,44 @@
 import { DateToStrPipe } from './date-to-str.pipe';
 
 describe('DateToStrPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DateToStrPipe();
-    expect(pipe).toBeTruthy();
+  let pipe: DateToStrPipe;
+
+  beforeEach(() => {
+    pipe = new DateToStrPipe();
+    jasmine.clock().install();
+    const mockDate = new Date(2026, 1, 22, 8);
+    jasmine.clock().mockDate(mockDate);
+  });
+
+  afterEach(() => {
+    jasmine.clock().uninstall();
+  });
+
+  it('show 07:00', () => {
+    expect(pipe.transform('2026-02-22T07:00:00.000')).toBe('07:00');
+  });
+
+  it('show yesterday', () => {
+    expect(pipe.transform('2026-02-21T08:00:00.000')).toBe('вчера');
+  });
+
+  it('show yesterday', () => {
+    expect(pipe.transform('2026-02-21T07:00:00.000')).toBe('вчера');
+  });
+
+  it('show yesterday', () => {
+    expect(pipe.transform('2026-02-21T01:00:00.000')).toBe('вчера');
+  });
+
+  it('show date', () => {
+    expect(pipe.transform('2026-02-20T08:00:00.000')).toBe('20.02');
+  });
+
+  it('show date', () => {
+    expect(pipe.transform('2026-02-20T07:00:00.000')).toBe('20.02');
+  });
+
+  it('show full date', () => {
+    expect(pipe.transform('2025-02-20T07:00:00.000')).toBe('20.02.2025');
   });
 });
