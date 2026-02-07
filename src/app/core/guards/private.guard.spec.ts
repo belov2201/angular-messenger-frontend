a91b1af308772bf9bde83d5d34a4a7a882e1ee44
Андрей Белов <andrew.belov2201@yandex.ru>
2026-02-07 16:34:56 +0300

    Тестирование guards

--- src/app/core/guards/private.guard.spec.ts
+++ src/app/core/guards/private.guard.spec.ts
@@ -1,17 +1,47 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
-
+import {
+  ActivatedRouteSnapshot,
+  GuardResult,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { privateGuard } from './private.guard';
+import { getSharedProviders } from 'testing/get-shared-providers';
+import { firstValueFrom, Observable, throwError } from 'rxjs';
+import { UserService } from '../store/user/user.service';
 
 describe('privateGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) =>
-    TestBed.runInInjectionContext(() => privateGuard(...guardParameters));
-
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        ...getSharedProviders(),
+        { provide: Router, useValue: { createUrlTree: jasmine.createSpy('createUrlTree') } },
+      ],
+    });
   });
 
-  it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
+  it('should be return true', async () => {
+    const result = await firstValueFrom(
+      TestBed.runInInjectionContext(() =>
+        privateGuard({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot),
+      ) as Observable<GuardResult>,
+    );
+
+    expect(result).toBeTrue();
+  });
+
+  it('should be redirect to auth', async () => {
+    const router = TestBed.inject(Router);
+    const userService = TestBed.inject(UserService) as jasmine.SpyObj<UserService>;
+    userService.getUserData.and.returnValue(throwError(() => new Error('Unauthorized')));
+
+    await firstValueFrom(
+      TestBed.runInInjectionContext(() =>
+        privateGuard({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot),
+      ) as Observable<UrlTree>,
+    );
+
+    expect(router.createUrlTree).toHaveBeenCalledOnceWith(['/auth']);
   });
 });
--- src/app/core/guards/public.guard.spec.ts
+++ src/app/core/guards/public.guard.spec.ts
@@ -1,17 +1,48 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
-
+import {
+  ActivatedRouteSnapshot,
+  GuardResult,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { publicGuard } from './public.guard';
+import { getSharedProviders } from 'testing/get-shared-providers';
+import { firstValueFrom, Observable, throwError } from 'rxjs';
+import { UserService } from '../store/user/user.service';
 
 describe('publicGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) =>
-    TestBed.runInInjectionContext(() => publicGuard(...guardParameters));
-
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        ...getSharedProviders(),
+        { provide: Router, useValue: { createUrlTree: jasmine.createSpy('createUrlTree') } },
+      ],
+    });
   });
 
-  it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
+  it('should be redirect to main', async () => {
+    const router = TestBed.inject(Router);
+
+    await firstValueFrom(
+      TestBed.runInInjectionContext(() =>
+        publicGuard({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot),
+      ) as Observable<UrlTree>,
+    );
+
+    expect(router.createUrlTree).toHaveBeenCalledOnceWith(['/']);
+  });
+
+  it('should return true', async () => {
+    const userService = TestBed.inject(UserService) as jasmine.SpyObj<UserService>;
+    userService.getUserData.and.returnValue(throwError(() => new Error('Unauthorized')));
+
+    const result = await firstValueFrom(
+      TestBed.runInInjectionContext(() =>
+        publicGuard({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot),
+      ) as Observable<GuardResult>,
+    );
+
+    expect(result).toBeTrue();
   });
 });
